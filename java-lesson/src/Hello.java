7f76ef2e94c8f19ddde44e3cc81356db9de34198
COMNIC01 <COMNIC01@DESKTOP-VI1SO5C>
2019-06-17 15:21:09 +0900

    test終わりました

--- java-lesson/src/Hello.java
+++ java-lesson/src/Hello.java
@@ -3,6 +3,10 @@ public class Hello {
 	public static void main(String[] args) {
 //		lesson1 lesson = new lesson1();
 //		lesson.showIf();
-	
+		for (int i = 0; i < 5; i++) {
+			if (i%2==0) {
+				System.out.println(i);
+			}
+		}		
 	}
 }
