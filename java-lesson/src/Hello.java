771c52e94d5b6161e7a771b86aac48d4c5633af6
COMNIC01 <COMNIC01@DESKTOP-VI1SO5C>
2019-06-17 15:12:40 +0900

    if文とfor文消しました

--- java-lesson/src/Hello.java
+++ java-lesson/src/Hello.java
@@ -3,10 +3,6 @@ public class Hello {
 	public static void main(String[] args) {
 //		lesson1 lesson = new lesson1();
 //		lesson.showIf();
-		for (int i = 0; i < 5; i++) {
-			if (i%2==0) {
-				System.out.println(i);
-			}
-		}		
+	
 	}
 }
